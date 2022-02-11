13fde9a049dd8276cd74f6d8899d1d23aea62213
Juan C. Moreno B <juan.carlos.moreno.banda@hotmail.com>
2022-02-11 16:10:58 -0500

    configuration server with express

---
+++ index.js
@@ -0,0 +1,11 @@
+const http = require("http");
+const app = require("./server");
+const config = require("./server/config");
+
+const { port, hostname } = config;
+
+const server = http.createServer(app);
+
+server.listen(port, hostname, () => {
+  console.log(`server running in http://${hostname}:${port}`);
+});
