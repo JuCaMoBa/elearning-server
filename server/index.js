f85656d5589e11c2fd4fd7d7e04f618b69a0ec7b
Juan C. Moreno B <juan.carlos.moreno.banda@hotmail.com>
2022-02-11 16:31:44 -0500

    Handler Route not found and all errors

--- server/index.js
+++ server/index.js
@@ -6,4 +6,29 @@ app.get("/", (req, res, next) => {
   res.send("hello Word");
 });
 
+// Handler Error route not found
+app.use((req, res, next) => {
+  const message = "Error. Route Not Found";
+  const statusCode = 404;
+  next({
+    statusCode,
+    message,
+  });
+});
+
+// Handler all Error
+app.use((error, req, res, next) => {
+  const { message = "", name = "" } = error;
+  let { statusCode = 500 } = error;
+  console.log(error.name);
+  if (name === "ValidationError") {
+    statusCode = 422;
+  }
+  res.status(statusCode);
+  res.json({
+    message,
+    error,
+  });
+});
+
 module.exports = app;
