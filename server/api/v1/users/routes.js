16c89c3b6f4c19718be2a0e5a9516fd2a528691d
Juan C. Moreno B <juan.carlos.moreno.banda@hotmail.com>
2022-02-11 19:04:43 -0500

    add routes for database users

--- server/api/v1/users/routes.js
+++ server/api/v1/users/routes.js
@@ -1,7 +1,26 @@
 const express = require("express");
+const controller = require("./controller");
+const { auth } = require("../auth");
+const { sanitizers } = require("./model");
 
-const router = express.Router({
-  mergeParams: true,
-});
+const router = express.Router();
+
+/*
+ *
+ * /api/users/signin     POST   - Signin
+ * /api/users/signup     POST   - Signup
+ * /api/users/profile    GET    - Get the profile of the current user
+ * /api/users/profile    PUT    - Update the profile of the current user
+ * /api/users/profile    PATCH  - Update the profile of the current user
+ */
+
+router.route("/signin").post(controller.signin);
+router.route("/signup").post(sanitizers, controller.signup);
+
+router
+  .route("/profile")
+  .get(auth, controller.profile)
+  .put(auth, controller.update)
+  .patch(auth, controller.update);
 
 module.exports = router;
