ec377f0ffa782d6869a851d2ee6b6ca640e62ec5
Juan C. Moreno B <juan.carlos.moreno.banda@hotmail.com>
2022-02-11 19:04:13 -0500

    add .env variable for database connection

--- server/config/index.js
+++ server/config/index.js
@@ -3,6 +3,16 @@ require("dotenv").config();
 const config = {
   port: process.env.PORT || 4000,
   hostname: process.env.HOSTNAME || "127.0.0.1",
+  token: {
+    secret: process.env.TOKEN_SECRET,
+    expires: process.env.TOKEN_EXPIRES,
+  },
+  database: {
+    protocol: process.env.DATABASE_PROTOCOL,
+    url: process.env.DATABASE_URL,
+    username: process.env.DATABASE_USERNAME,
+    password: process.env.DATABASE_PASSWORD,
+  },
 };
 
 module.exports = config;
